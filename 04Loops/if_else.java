1c411cb72bd85a9ae1dbe9951e692c325819559a
Abhishek Kumar <abhishekbth99@gmail.com>
2022-07-07 16:52:39 +0530

    Add files via upload

---
+++ 04Loops/if_else.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class if_else {
+    public static void main(String args[]){
+        Scanner scan=new Scanner(System.in);
+        
+        int i=scan.nextInt();
+        int rem= i%2;
+        
+        if(rem==0)
+        {
+        System.out.println("Even Number");
+        }
+        else
+        {
+        System.out.println("Odd Number");
+        }
+
+    }
+    
+}
