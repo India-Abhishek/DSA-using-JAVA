8ca2e904bd7214e7d9947ff7d53c15649c28afac
Abhishek Kumar <abhishekbth99@gmail.com>
2022-07-07 16:53:13 +0530

    Add files via upload

---
+++ 04Loops/Patterns.java
@@ -0,0 +1,22 @@
+// Printing n*n type of patterns.
+
+import java.util.Scanner;
+
+public class Patterns {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int i=1;
+        while (i<=n){
+            int j=1;
+            while (j<=n){
+                System.out.print(j);
+                j++;
+            }
+            System.out.println();
+            i++;    
+        } 
+        n--;    
+    }
+    
+}
